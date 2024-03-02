c7e3690768a983de1d03e3e703e57a9b6d4fe1f1
y109240 <161835317+y109240@users.noreply.github.com>
2024-03-02 09:38:43 +0900

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,10 +1,20 @@
 
 # 구구단 만들기
 
+# def print_gugudan(dan):
+#   print("{}단".format(dan))
+#   for i in range(1, 10) :
+#     result = dan * i
+#     print("{} * {} = {}".format(dan, i, result))
+
+# print_gugudan(2)
+
+n = input("몇단을 출력할까요? : ")
+
 def print_gugudan(dan):
   print("{}단".format(dan))
   for i in range(1, 10) :
     result = dan * i
     print("{} * {} = {}".format(dan, i, result))
 
-print_gugudan(2)
+print_gugudan(int((n)))
