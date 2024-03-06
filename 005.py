7b76ecf1028c96643c04d0eeb4aa8c9ca5c3399b
y109240 <161835317+y109240@users.noreply.github.com>
2024-03-06 22:25:11 +0900

    Add files via upload

---
+++ 005.py
@@ -0,0 +1,17 @@
+# 구구단표 만들기
+print(format("구구단표",">20s"))
+
+# 표 머리 출력
+print("  |", end = "")
+for j in range(1, 10):
+  print("  ", j, end = "")
+# 새로운 행 삽입
+print()
+print("-----------------------------------------")
+
+# 구구단 표 출력
+for i in range(1, 10):
+  print(i, "|", end="")
+  for j in range(1, 10):
+    print(format(i*j,">4d"), end="")
+  print()
