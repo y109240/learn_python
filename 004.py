18670ea2fd27fc1e4941edefb820c8540f345f66
y109240 <161835317+y109240@users.noreply.github.com>
2024-03-07 23:41:02 +0900

    Update 004.py

--- 004.py
+++ 004.py
@@ -34,3 +34,30 @@ def computer_guess(x):
   print(f"The computer guessd your number, {guess} correctly.")
 
 computer_guess(100)
+
+# 제한이있는 하이앤로우
+hit_number = random.randint(1, 1000)
+class CountError(Exception):
+  pass
+count = 0
+passfail = False
+while passfail == False:
+  try:
+    user = int(input("1~999 사이의 숫자 중에서 맞혀보세요 : "))
+    count += 1
+    if hit_number == user :
+      print(f"[{count}번째] 정답입니다!")
+      passfail = True
+    if count > 5:
+      raise CountError
+    elif hit_number > user:
+      print(f"[{count}번째] 숫자가 작습니다. 다시 맞혀보세요.")
+    elif hit_number < user:
+      print(f"[{count}번째] 숫자가 큽니다. 다시 맞혀보세요.")
+    if isinstance(user, int) != True:
+      raise ValueError
+  except ValueError:
+    print("잘못된 값을 입력하였습니다. 숫자를 입력해주세요.")
+  except CountError:
+    print("20번의 기회가 모두 끝났습니다.")
+    break
