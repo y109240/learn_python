ea1b332aea1b2a0dacc64c679e1fbc5471db409c
y109240 <161835317+y109240@users.noreply.github.com>
2024-03-24 22:29:10 +0900

    Update 010.py

--- 010.py
+++ 010.py
@@ -13,3 +13,36 @@ elif num != 1:
 
 # 더 효율적으로 찾는 방법
 # 모든 약수들을 찾아서 나열하는 방법
+
+def find_divisors(num):
+  return [x for x in range(1, num+1) if num % x == 0]
+  # divisors = []
+  # for x in range(1, num+1):
+  #   if num % x == 0:
+  #     divisors.append(x)
+  # return divisors
+
+def is_prime(num):
+  if num == 1:
+    return False
+  else:
+    for i in range(2, num):
+      if num % i == 0:
+        return False
+    else:
+      return True
+
+def find_prime_divisors(num):
+  return [x for x in find_divisors(num) if is_prime(x)]
+  # prime_divisors = []
+  # for x in find_divisors(num):
+  #   if is_prime(x):
+  #     prime_divisors.append(x)
+  # return prime_divisors
+
+try:
+  num = int(input("자연수를 입력해 주세요 : "))
+except ValueError:
+  print("자연수가 아닙니다.")
+else:
+  print(find_prime_divisors(num))
