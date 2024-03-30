e4e027ea7de5594f7bea9dd62ab148bfc7a5cf86
y109240 <161835317+y109240@users.noreply.github.com>
2024-03-31 00:08:16 +0900

    Update 020.py

--- 020.py
+++ 020.py
@@ -8,6 +8,7 @@ from pydub.playback import play
 import requests
 from collections.abc import Callable
 
+
 def speech_to_text() -> str:
   """음성 인식
   마이크로 사용자의 음성을 듣고 결과를 문자열로 반환
@@ -16,16 +17,21 @@ def speech_to_text() -> str:
   Returns:
     str: 음성 인식 결과 문자열. 실패시 빈 문자열 반환.
   """
+
   r = sr.Recognizer()
   microphone = sr.Microphone()
   with microphone as source:
     r.adjust_for_ambient_noise(source)
-    print("음성 인식 대기중")
+    print("음성 명령 대기중")
     audio = r.listen(source)
-  text = r.recognize_google(audio, language="ko")
-  print("인식 결과:", text)
-  return text if text else ""
-
+  try:
+    user_command = r.recognize_google(audio, language="ko")
+  except Exception as e:
+    print("인식할 수 없습니다.", e)
+    return ""
+  else:
+    print("인식 결과:", user_command)
+    return user_command
 
 
 def text_to_speech(text: str) -> None:
@@ -34,6 +40,7 @@ def text_to_speech(text: str) -> None:
   Args:
     text (str): 음성 합성할 문자열
   """
+
   print(text)
   tts = NaverTTS(text, lang="ko")
   fp = BytesIO()
@@ -41,7 +48,6 @@ def text_to_speech(text: str) -> None:
   fp = BytesIO(fp.getvalue())
   my_sound = AudioSegment.from_file(fp, format="mp3")
   play(my_sound)
-
 
 
 def report_daytime(user_command: str) -> None:
@@ -61,19 +67,14 @@ def report_daytime(user_command: str) -> None:
     "런던": "Europe/London",
   }
 
-  found = [found for found in cities_dict if found in user_command]
-  if found:
-    cities = found[0]
-  else:
-    cities = "서울"
+  city = find_keyword(cities_dict.keys(), user_command, "서울")
 
-  tz = timezone(cities_dict[cities])
+  tz = timezone(cities_dict[city])
   today = datetime.today().astimezone(tz)
   if "시간" in user_command:
-    return text_to_speech(f"현재 {cities}의 시간은 {today.hour}시 {today.minute}분 입니다.")
+    return text_to_speech(f"현재 {city}의 시간은 {today.hour}시 {today.minute}분 입니다.")
   elif "날짜" in user_command:
-    return text_to_speech(f"오늘 {cities}은/는 {today.year}년 {today.month}월 {today.day}일 입니다.")
-
+    return text_to_speech(f"오늘 {city}은/는 {today.year}년 {today.month}월 {today.day}일 입니다.")
 
 
 def report_weather(user_command: str) -> None:
@@ -97,21 +98,15 @@ def report_weather(user_command: str) -> None:
     "런던": "london",
   }
 
-  user_command = speech_to_text()
-  found = [found for found in cities_dict if found in user_command]
-  if found:
-    cities = found[0]
-  else:
-    cities = "서울"
-
   if "날씨" in user_command:
-    requests_url = f"{BASE_URL}?appid={API_KEY}&q={cities_dict[cities]}&lang=Kr"
+    city = find_keyword(cities_dict.keys(), user_command, "서울")
+    requests_url = f"{BASE_URL}?appid={API_KEY}&q={cities_dict[city]}&lang=Kr"
     response = requests.get(requests_url)
     if response.status_code == 200:
       data = response.json()
       weather = data["weather"][0]["description"]
       temperaturs = round(data["main"]["temp"]-273.15, 2)
-      text_to_speech(f"현재 {cities}의 날씨는 {weather}, 온도는 {temperaturs}도 입니다.")
+      text_to_speech(f"현재 {city}의 날씨는 {weather}, 온도는 {temperaturs}도 입니다.")
     else:
       text_to_speech("날씨 정보를 받아오지 못했습니다.")
 
@@ -125,14 +120,12 @@ def find_keyword(keywords: list[str], sentence: str, default: str = "") -> str:
   Returns:
     str: 문장 안에서 처음 발견한 키워드. 없을 경우 default 반환.
   """
-
-  keywords = ["시간", "날짜", "날씨"]
-  sentence = speech_to_text()
   for k in keywords:
     if k in sentence:
       return k
-    else:
-      return default
+  else:
+    return default
+# "로스앤젤레스와 서울의 시간"이라고 하면 서울이 먼저 출력되는 문제
 
 
 # [보충] 여기서 Callable[[str], None]은 str을 입력으로 받고 None을 반환하는 함수의 타입힌트입니다.
@@ -147,19 +140,31 @@ def listen_and_report(command_callbacks: dict[str, Callable[[str], None]]) -> bo
     bool: 프로그램을 계속 진행할 경우 True, 종료할 경우 False
   """
 
-  speech_to_text()
-  if "종료" in speech_to_text():
-    text_to_speech("종료합니다.")
+  user_command = speech_to_text()
+  if "종료" in user_command:
+    text_to_speech("음성 인터페이스를 종료합니다.")
     return False
+  # elif "시간" in user_command:
+  #   report_daytime(user_command)
+  #   return True
+  # elif "날짜" in user_command:
+  #   report_daytime(user_command)
+  #   return True
+  # elif "날짜" in user_command:
+  #   report_weather(user_command)
+  #   return True
+  for k in command_callbacks.keys():
+    if k in user_command:
+      command_callbacks[k](user_command)
+      return True
   else:
-    for i in command_callbacks:
-      if i in speech_to_text():
-        command_callbacks[i]
-        return True
+    text_to_speech("알 수 없는 명령입니다.")
+    return True
 
 
 command_callbacks = {
   "시간": report_daytime,
+  "날짜": report_daytime,
   "날씨": report_weather,
 }
 
@@ -167,3 +172,4 @@ while listen_and_report(command_callbacks):
   continue
 
 # 제대로 실행되지 않음
+# 덧붙일 내용 : 시간과 날짜, 도시를 두개이상 말할때 순서 정렬
