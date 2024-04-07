0c34e6f48cd9ed4fe8e5b06e452dbecf1e3b6c85
y109240 <161835317+y109240@users.noreply.github.com>
2024-04-07 23:13:46 +0900

    Update 009.py

--- 009.py
+++ 009.py
@@ -36,4 +36,29 @@ for j in range(10):
     t.right(144)
   t.left(36)
 
+
+# 이런것도 할수있다 - 장미그리기
+t.speed(50)
+t.bgcolor("black")
+t.color("pink")
+t.setup(650, 900)
+# 꽃 그리기
+for i in range(1, 201):
+    t.pensize(1/50)
+    t.forward(i)
+    t.left(65)
+# 줄기 그리기
+t.color("lightblue")
+t.setheading(270)
+for i in range(50):
+    t.pensize(25-i/2)
+    t.forward(i/4)
+# 잎 그리기
+t.color("yellowgreen")
+t.setheading(60)
+for x in range(100):
+    t.pensize(100-x)
+    t.forward(x/10)
+t.ht()
+
 turtle.done()
