827e8edc1b11c5266d772cffa47677303ae3c4fc
y109240 <161835317+y109240@users.noreply.github.com>
2024-06-12 17:00:59 +0900

    Add files via upload

--- Django01.py
+++ Django01.py
@@ -1,15 +1,18 @@
 ## Boookmark ##
 
-# models.py
+# models.py : 테이블 생성
 from django.db import models
 class Bookmark(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
     url = models.URLField("url", unique=True)
     def __str__(self):
         return self.title
-   
 
-# admin.py
+# dos (cmd)
+# python manage.py makemigrations
+# python manage.py migrate
+
+# admin.py : 관리자 페이지 설정
 from django.contrib import admin
 from bookmark.models import Bookmark
 class BookmarkAdmin(admin.ModelAdmin):
@@ -53,3 +56,42 @@ def detail(request):
     dto = Bookmark.objects.get(url=url)
     template = loader.get_template("detail.html")
     return HttpResponse(template.render({"dto":dto}, request))
+
+
+## templates (Package)
+
+# detail.html
+# <!DOCTYPE html>
+# <html>
+# 	<head>
+# 		<meta charset="UTF-8">
+# 		<title> Bookmark </title>
+# 	</head>
+# 	<body>
+# 		<h2> {{ dto.title }}  </h2>
+# 		<br><br>
+# 		URL : <a href="{{ dto.url }}"> {{ dto.title }} </a>
+# 	</body>
+# </html>
+
+# home.html
+# {% load static %}
+# <!DOCTYPE html>
+# <html>
+# 	<head>
+# 		<meta charset="UTF-8">
+# 		<title> Bookmark </title>
+# 		<link rel="stylesheet" type="text/css" href="{% static 'css/style.css' %}">
+# 	</head>
+# 	<body background="{% static 'images/a.jpg' %}">
+# 		<h2> 북마크 </h2>
+# 		<br><br>
+# 		{{ urlcount }}개의 북마크가 있습니다. <br>
+# 		<br>
+# 		<ul>
+# 			{% for row in urllist %}	
+# 				<li> <a href="detail?url={{ row.url }}"> {{ row.title }} </a> </li> <br>
+# 			{% endfor %}
+# 		</ul>	
+# 	</body>
+# </html>
