9fbd345755649963279c2ea503464f76840404bf
y109240 <161835317+y109240@users.noreply.github.com>
2024-06-18 23:55:56 +0900

    Update Django03.py

--- Django03.py
+++ Django03.py
@@ -28,25 +28,151 @@ from django.urls.conf import path
 from guestbook import views
 
 urlpatterns = [
-    path( "", views.guestbook, name="guestbook" ),
+    path("", views.guestbook, name="guestbook"),
+    # localhost:8000/guestbook
+    path("write", views.write, name="write"),
+    # localhost:8000/guestbook/write
+    path("writepro", views.writepro, name="writepro"),
+    # localhost:8000/guestbook/writepro
+    path("passwdck", views.passwdck, name="passwdck"),
+    # localhost:8000/guestbook/passwdck
+    path("delete", views.delete, name="delete"),
+    # localhost:8000/guestbook/delete
+    path("update", views.update, name="update"),
+    # localhost:8000/guestbook/update
 ]
 
 
 # views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from guestbook.models import Guestbook
 from django.template import loader
 from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
+from django.utils.decorators import method_decorator
 
-def guestbook( request ):
-    gblist = Guestbook.objects.order_by( "-idx" )
-    gbcount = Guestbook.objects.count()
-    context = {
-        "gblist" : gblist,
-        "gbcount" : gbcount
-    }
-    template = loader.get_template( "guestbook.html" )
-    return HttpResponse( template.render( context, request ) )
+# def guestbook(request):
+#     gblist = Guestbook.objects.order_by("-idx")
+#     gbcount = Guestbook.objects.count()
+#     context = {
+#         "gblist" : gblist,
+#         "gbcount" : gbcount
+#         }
+#     template = loader.get_template("guestbook.html")
+#     return HttpResponse(template.render(context, request))
+class ListView(View):
+    def get(self, request):
+        gblist = Guestbook.objects.order_by("-idx")
+        gbcount = Guestbook.objects.count()
+        context = {
+            "gblist" : gblist,
+            "gbcount" : gbcount
+            }
+        template = loader.get_template("guestbook.html")
+        return HttpResponse(template.render(context, request))
+
+# def write(request):
+#     template = loader.get_template("write.html")
+#     return HttpResponse(template.render({}, request))
+# @csrf_exempt
+# def writepro(request):
+#     dto = Guestbook(
+#         name = request.POST["name"],
+#         email = request.POST["email"],
+#         passwd = request.POST["passwd"],
+#         content = request.POST["content"]
+#         )
+#     dto.save()
+#     return redirect("guestbook")    
+class WriteView(View):
+    @method_decorator( csrf_exempt )
+    def dispatch(self, request, *args, **kwargs):
+        return super(WriteView, self).dispatch(request, *args, **kwargs)
+    def get(self, request):
+        template = loader.get_template("write.html")
+        return HttpResponse(template.render({}, request))
+    def post(self, request):
+        dto = Guestbook(
+            name = request.POST["name"],
+            email = request.POST["email"],
+            passwd = request.POST["passwd"],
+            content = request.POST["content"]
+        )
+        dto.save()
+        return redirect("/guestbook")
+
+# @csrf_exempt
+# def passwdck(request):
+#     idx = request.POST["idx"]
+#     passwd = request.POST["passwd"]
+#     dto = Guestbook.objects.get(idx=idx)
+#     if passwd == dto.passwd : 
+#         template = loader.get_template("edit.html")
+#         context = {
+#             "dto" : dto
+#             }
+#         return HttpResponse(template.render(context, request))
+#     else:
+#         return redirect("guestbook")    
+class PasswdView(View):
+    @method_decorator( csrf_exempt )
+    def dispatch(self, request, *args, **kwargs):
+        return super(PasswdView, self).dispatch(request, *args, **kwargs)
+    def post(self, request):
+        idx = request.POST["idx"]
+        passwd = request.POST["passwd"]
+        dto = Guestbook.objects.get(idx=idx)
+        if passwd == dto.passwd:
+            template = loader.get_template("edit.html")
+            context = {
+                "dto" : dto
+                }
+            return HttpResponse(template.render(context, request))
+        else:
+            return redirect("/guestbook")
+ 
+# def delete(request):
+#     idx = request.GET["idx"]
+#     dto = Guestbook.objects.get(idx=idx)
+#     dto.delete()
+#     return redirect("guestbook")
+# @csrf_exempt
+# def update(request):
+#     idx = request.POST["idx"]
+#     dto = Guestbook.objects.get(idx=idx)
+#     newdto = Guestbook(
+#         idx = dto.idx,
+#         name = dto.name,
+#         email = request.POST["email"],
+#         passwd = request.POST["passwd"],
+#         content = request.POST["content"],
+#         postdate = dto.postdate
+#         )
+#     newdto.save()
+#     return redirect("guestbook")
+class UpdateView(View):
+    @method_decorator( csrf_exempt )
+    def dispatch(self, request, *args, **kwargs):
+        return super(UpdateView, self).dispatch(request, *args, **kwargs)
+    def get(self, request):
+        idx = request.GET["idx"]
+        dto = Guestbook.objects.get(idx=idx)
+        dto.delete()
+        return redirect("/guestbook")
+    def post(self, request):
+        idx = request.POST["idx"]
+        dto = Guestbook.objects.get(idx=idx)
+        newdto = Guestbook(
+            idx = dto.idx,
+            name = dto.name,
+            email = request.POST["email"],
+            passwd = request.POST["passwd"],
+            content = request.POST["content"],
+            postdate = dto.postdate
+        )
+        newdto.save()
+        return redirect("/guestbook")
 
 
 # guestbook.html
